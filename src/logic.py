c9254c2c32d047532b1c945bf24d38749a619170
Eraj Gillani <eraj.gillani@hotmail.com>
2022-07-11 21:21:28 -0400

    trying to fix main branch

--- src/logic.py
+++ src/logic.py
@@ -1,9 +1,5 @@
-from typing import List
-import global_variables as gv
-#have a library with our strategy
-import strategy
-import strategy_open
-import copy
+import random
+from typing import List, Dict
 
 """
 This file can be a nice home for your Battlesnake's logic and helper functions.
@@ -21,10 +17,10 @@ def get_info() -> dict:
     """
     return {
         "apiversion": "1",
-        "author": "Sir-Pent",  # TODO: Your Battlesnake Username
-        "color": "#202252",  # TODO: Personalize
-        "head": "moustache",  # TODO: Personalize
-        "tail": "shiny",  # TODO: Personalize
+        "author": "Sir-Pent",
+        "color": "#202252",
+        "head": "moustache",
+        "tail": "shiny"
     }
 
 
@@ -51,83 +47,34 @@ def choose_move(data: dict) -> str:
     # print(f"My Battlesnakes head this turn is: {my_head}")
     # print(f"My Battlesnakes body this turn is: {my_body}")
 
-    snake_walls = []
-    other_snakes_wall = []
-    food_and_snakes = []
-    all_food = []
-    board = []
-    length_of_each_snake = []
-    #populating an empty board 
-    for row in range(gv.BOARD_MAX_X):
-        board.append([])
-        for column in range(gv.BOARD_MAX_Y):
-            board[row].append(1)
+    possible_moves = ["up", "down", "left", "right"]
 
-    #populating snake parts
-    #have list of our other snakes body parts that will act as our walls
-    all_snake_body_parts = strategy.other_snakes(data)
-    for part in all_snake_body_parts:
-        snake_walls.append((part["x"], part["y"]))
-        board[part["x"]][part["y"]] = 0
-        if part not in data["you"]["body"]:
-            other_snakes_wall.append((part["x"], part["y"]))
+    # Step 0: Don't allow your Battlesnake to move back on it's own neck.
+    possible_moves = _avoid_my_neck(my_body, possible_moves)
 
+    # TODO: Step 1 - Don't hit walls.
+    # Use information from `data` and `my_head` to not move beyond the game board.
+    # board = data['board']
+    # board_height = ?
+    # board_width = ?
 
-    #populating all the food
-    all_food_data = data["board"]["food"]
-    for food in all_food_data:
-        all_food.append((food["x"], food["y"]))
-        board[food["x"]][food["y"]] = 0
+    # TODO: Step 2 - Don't hit yourself.
+    # Use information from `my_body` to avoid moves that would collide with yourself.
 
-    food_and_snakes = copy.deepcopy(snake_walls)
-    for food in all_food_data:
-        food_and_snakes.append((food["x"], food["y"]))
+    # TODO: Step 3 - Don't collide with others.
+    # Use information from `data` to prevent your Battlesnake from colliding with others.
 
-    curr_number_of_snakes = len(data["board"]["snakes"])
-    snakes = data["board"]["snakes"]
-    our_length = data["you"]["length"]
-    curr_total_length = 0
-    #get length of each snake
-    for snake in snakes:
-        if snake["name"] != data["you"]["name"]:
-            length_of_each_snake.append(snake["length"])
+    # TODO: Step 4 - Find food.
+    # Use information in `data` to seek out and find food.
+    # food = data['board']['food']
 
-    curr_total_length = len(other_snakes_wall)
-    avg_length = (curr_total_length)/curr_number_of_snakes
-    ##########3 or less Snakes###############
-    if curr_number_of_snakes <= 2:
-        if data["you"]["health"] > 90 and our_length < avg_length:
-            move = strategy_open.go_to_open(data, board, food_and_snakes, snake_walls, other_snakes_wall)
-        if move is not None:
-            print(f"MOVE: {move}")
-            return {"move": move}
-        else: #go for food
-            move = strategy.go_for_food_Dij(data,snake_walls, other_snakes_wall, all_food)
-        if move is None:
-            move = strategy_open.go_to_open(data, board, food_and_snakes, snake_walls, other_snakes_wall)
-            if move is not None:
-                print(f"MOVE: {move}")
-                return {"move": move}
-    else:#multiple snakes 
-        if data["you"]["health"] > 95 and our_length < 5:
-            move = strategy_open.go_to_open(data, board, food_and_snakes, snake_walls, other_snakes_wall)
-        if move is not None:
-            print(f"MOVE: {move}")
-            return {"move": move}
-        else: #go for food
-            move = strategy.go_for_food_Dij(data,snake_walls, other_snakes_wall, all_food)
-        if move is None:
-            move = strategy_open.go_to_open(data, board, food_and_snakes, snake_walls, other_snakes_wall)
-            if move is not None:
-                print(f"MOVE: {move}")
-                return {"move": move}
-    #######################################
+    # Choose a random direction from the remaining possible_moves to move in, and then return that move
+    move = random.choice(possible_moves)
+    # TODO: Explore new strategies for picking a move that are better than random
 
+    print(f"{data['game']['id']} MOVE {data['turn']}: {move} picked from all valid options in {possible_moves}")
 
-    print(f"MOVE: {move}")
-    return {"move": move}
-
-
+    return move
 
 
 def _avoid_my_neck(my_body: dict, possible_moves: List[str]) -> List[str]:
