74d088e4441ff4f4ae725ae532ae3ab0198c4f6f
Eraj Gillani <eraj.gillani@hotmail.com>
2022-07-16 15:03:54 -0400

    fixed indentation errors

--- src/main.py
+++ src/main.py
@@ -98,30 +98,30 @@ def handle_move():
     if curr_number_of_snakes <= 2:
         if data["you"]["health"] > 90 and our_length < avg_length:
             move = strategy_open.go_to_open(data, board, food_and_snakes, snake_walls, other_snakes_wall)
-        if move is not None:
-            print(f"MOVE: {move}")
-            return {"move": move}
-        else: #go for food
-            move = strategy.go_for_food_Dij(data,snake_walls, other_snakes_wall, all_food)
-        if move is None:
-            move = strategy_open.go_to_open(data, board, food_and_snakes, snake_walls, other_snakes_wall)
             if move is not None:
                 print(f"MOVE: {move}")
                 return {"move": move}
+        else: #go for food
+            move = strategy.go_for_food_Dij(data,snake_walls, other_snakes_wall, all_food)
+            if move is None:
+                move = strategy_open.go_to_open(data, board, food_and_snakes, snake_walls, other_snakes_wall)
+                if move is not None:
+                    print(f"MOVE: {move}")
+                    return {"move": move}
     #multiple snakes 
     else:
         if data["you"]["health"] > 95 and our_length < 5:
             move = strategy_open.go_to_open(data, board, food_and_snakes, snake_walls, other_snakes_wall)
-        if move is not None:
-            print(f"MOVE: {move}")
-            return {"move": move}
-        else: #go for food
-            move = strategy.go_for_food_Dij(data,snake_walls, other_snakes_wall, all_food)
-        if move is None:
-            move = strategy_open.go_to_open(data, board, food_and_snakes, snake_walls, other_snakes_wall)
             if move is not None:
                 print(f"MOVE: {move}")
                 return {"move": move}
+        else: #go for food
+            move = strategy.go_for_food_Dij(data,snake_walls, other_snakes_wall, all_food)
+            if move is None:
+                move = strategy_open.go_to_open(data, board, food_and_snakes, snake_walls, other_snakes_wall)
+                if move is not None:
+                    print(f"MOVE: {move}")
+                    return {"move": move}
     #######################################
 
     print(f"MOVE: {move}")
